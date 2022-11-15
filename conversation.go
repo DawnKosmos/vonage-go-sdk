76e78c4ff4ddf9625e6f681055f7889d17e3d5de
DawnKosmos <martin_hottmann@yahoo.com>
2022-11-15 17:43:49 +0100

    MVP added

--- conversation.go
+++ conversation.go
@@ -1,6 +1,167 @@
 package vonage
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/vonage/vonage-go-sdk/internal/conversation"
+	"github.com/vonage/vonage-go-sdk/jwt"
+	"io"
+	"log"
+	"net/http"
+)
+
 /*
 DawnKosmos
 using version 1
 */
+
+var conversationUrl = "https://api.nexmo.com/v0.1/conversations"
+
+type Conversation struct {
+	Client *http.Client
+	Id     string
+	Href   string
+}
+
+/*
+PseudoCode
+NewConversation(Client http.Client, jwt Token)
+GetConversation(conversationId)
+Conversation
+	AddMember
+	IdInt
+*/
+
+func NewConversation(in conversation.CreateConversationRequest, client *http.Client, gen *jwt.Generator) (*Conversation, error) {
+	token, err := gen.GenerateToken()
+	if err != nil {
+		return nil, err
+	}
+
+	by, err := json.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("POST", conversationUrl, bytes.NewBuffer(by))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := requestAuth(client, req, token)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, failedRequest(resp)
+	}
+	var data conversation.CreateConversationResponse
+	if err := processResponse(resp, &data); err != nil {
+		return nil, err
+	}
+
+	return &Conversation{
+		Client: client,
+		Id:     data.Id,
+		Href:   data.Href,
+	}, nil
+}
+
+func (c *Conversation) AddMember(userId string, gen *jwt.Generator) (*conversation.CreateMemberResponse, error) {
+	token, err := gen.GenerateToken()
+	if err != nil {
+		return nil, err
+	}
+	body, err := json.Marshal(conversation.CreateMemberRequest{
+		Action: "join",
+		UserId: userId,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s/members", conversationUrl, c.Id), bytes.NewBuffer(body))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := requestAuth(c.Client, req, token)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 201 {
+		return nil, failedRequest(resp)
+	}
+
+	var data conversation.CreateMemberResponse
+	err = processResponse(resp, &data)
+	return &data, err
+}
+
+func CreateUser(input conversation.CreateUserRequest, client *http.Client, gen *jwt.Generator) (string, error) {
+	token, err := gen.GenerateToken()
+	if err != nil {
+		return "", err
+	}
+	body, err := json.Marshal(input)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.nexmo.com/v0.1/users"), bytes.NewBuffer(body))
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := requestAuth(client, req, token)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != 200 {
+		return "", failedRequest(resp)
+	}
+
+	var data conversation.CreateUserResponse
+	if err := processResponse(resp, &data); err != nil {
+		return "", err
+	}
+
+	return data.Id, nil
+}
+
+func CreateJWTToken(gen *jwt.Generator, username string) *jwt.Generator {
+	gen = gen.AddPath(jwt.Path{Path: "/*/conversations/**"}).AddPath(jwt.Path{Path: "/*/image/**"}).AddPath(jwt.Path{Path: "/*/session/**"})
+	return gen
+}
+
+// Help funcs
+func processResponse(resp *http.Response, result interface{}) error {
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error processing response: %v", err)
+		return err
+	}
+	err = json.Unmarshal(body, result)
+	if err != nil {
+		log.Printf("Error processing response: %v", err)
+		return err
+	}
+	return nil
+}
+
+func failedRequest(resp *http.Response) error {
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error processing response: %v", err)
+		return err
+	}
+	return errors.New(fmt.Sprintf("StatusCode: %d, Body: %s", resp.StatusCode, string(body)))
+}
+
+func requestAuth(client *http.Client, req *http.Request, token string) (*http.Response, error) {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+	return client.Do(req)
+}
---
+++ conversation_test.go
@@ -0,0 +1,64 @@
+package vonage
+
+import (
+	"fmt"
+	"github.com/vonage/vonage-go-sdk/internal/conversation"
+	"github.com/vonage/vonage-go-sdk/jwt"
+	"net/http"
+	"testing"
+)
+
+func TestConversationUserMember(t *testing.T) {
+	gen, err := jwt.NewGeneratorFromFilename("45a30037-7919-4919-a47d-2e354cce75d9", "internal/conversation/private.key")
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+	client := &http.Client{}
+
+	u1, err := CreateUser(conversation.CreateUserRequest{
+		Name:        "TestUser1",
+		DisplayName: "Alice",
+		ImageUrl:    "",
+	}, client, gen)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+	u2, err := CreateUser(conversation.CreateUserRequest{
+		Name:        "TestUser2",
+		DisplayName: "Bob",
+		ImageUrl:    "",
+	}, client, gen)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+
+	fmt.Println("Alice", u1)
+	fmt.Println("Bob", u2)
+
+	con, err := NewConversation(conversation.CreateConversationRequest{
+		Name:        "TestConversation",
+		DisplayName: "Kitty Club",
+		ImageUrl:    "",
+		Properties:  conversation.Property{60},
+	}, client, gen)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+	fmt.Println(con)
+
+	_, err = con.AddMember(u1, gen)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+	_, err = con.AddMember(u2, gen)
+	if err != nil {
+		fmt.Println(err)
+		t.FailNow()
+	}
+
+}
